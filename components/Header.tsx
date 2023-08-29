c27c40dd62c30e98b9a8f076c077c7289d8f785d
SomarGuy <diogo.ramos@me.com>
2023-08-29 19:52:38 -0400

    Refine Header layout for search and avatar alignment; add UserCircleIcon

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,10 +14,10 @@ function Header() {
             alt="Trello Logo"
             width={300}
             height={100}
-            className='w-44 md:w-56 pb-10 md:pb-0 object contain'
+            className='w-44 md:w-56 pb-10 md:pb-0 object-contain'
         />
 
-        <div className=''>
+        <div className='flex items-center space-x-5 flex-1 justify-end w-full'>
             {/* Search Box */}
             <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
                 <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
@@ -30,9 +30,15 @@ function Header() {
                     Search
                 </button>
             </form>
-            {/* Avatar */}
+
             <Avatar name='Diogo Ramos' round size="50" color="#0055D1"/>
             </div>
+        </div>
+
+        <div>
+            <p>
+                <UserCircleIcon className='inline-block h-10 w-10 text-[#0055D1] mr-1' />
+            </p>
         </div>
     </header>
   )
