f81ee35e12e433971edfaa12e1f20397269edfad
SomarGuy <diogo.ramos@me.com>
2023-08-29 19:58:12 -0400

    change background colour

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='bg-[#F5F6F8]'>{children}</body>
     </html>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -35,7 +35,7 @@ function Header() {
             </div>
         </div>
 
-        <div className='flex items-center justify-center px-5 md:py-5'>
+        <div className='flex items-center justify-center px-5 py-2 md:py-5'>
             <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl'>
                 <UserCircleIcon className='inline-block h-10 w-10 text-[#0055D1] mr-1' />
                 GPT is summarising your tasks for the day...
