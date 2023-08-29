664ad8f1835572c938c970112d2eb7f95afc2cc5
SomarGuy <diogo.ramos@me.com>
2023-08-29 17:04:54 -0400

    Add logo image to Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <Image
+            src="https://links.papareact.com/c2cdd5"
+            alt="Trello Logo"
+            width={300}
+            height={100}
+            className='w-44 md:w-56 pb-10 md:pb-0 object contain'
+        />
+    </header>
   )
 }
 
