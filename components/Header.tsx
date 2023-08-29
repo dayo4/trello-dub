c1faeabd6ac13affbccf1b3f9d2c76933eab00ed
SomarGuy <diogo.ramos@me.com>
2023-08-29 19:48:00 -0400

    Add user avatar using react-avatar package to Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import Image from 'next/image'
 import React from 'react'
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid"
+import Avatar from 'react-avatar'
 
 function Header() {
   return (
@@ -14,7 +17,7 @@ function Header() {
             className='w-44 md:w-56 pb-10 md:pb-0 object contain'
         />
 
-        <div>
+        <div className=''>
             {/* Search Box */}
             <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
                 <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
@@ -28,6 +31,7 @@ function Header() {
                 </button>
             </form>
             {/* Avatar */}
+            <Avatar name='Diogo Ramos' round size="50" color="#0055D1"/>
             </div>
         </div>
     </header>
