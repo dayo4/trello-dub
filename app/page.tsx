a3d088208bf121ade797cac08360da6929e2b03d
SomarGuy <diogo.ramos@me.com>
2023-08-29 16:51:12 -0400

    add header.tsx in components folder and updates home to include header

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main>
+      {/* Header */}
+      <Header/>
+      {/* Board */}
       <h1>Trello Clone</h1>
     </main>
   )
