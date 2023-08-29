c9cdd88796aec7d3be628b008259c2a1b97f618b
SomarGuy <diogo.ramos@me.com>
2023-08-29 17:14:21 -0400

    Update Header with improved styling for alignment, padding, and search box

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid"
 function Header() {
   return (
     <header>
+        <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
         <Image
             src="https://links.papareact.com/c2cdd5"
             alt="Trello Logo"
@@ -15,12 +16,19 @@ function Header() {
 
         <div>
             {/* Search Box */}
-            <form>
+            <form className='flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial'>
                 <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
-                <input type="text" placeholder='Search'/>
-                <button hidden>Search</button>
+                <input 
+                  type="text" 
+                  placeholder='Search' 
+                  className='flex-1 outline-none'
+                />
+                <button type="submit" hidden>
+                    Search
+                </button>
             </form>
             {/* Avatar */}
+            </div>
         </div>
     </header>
   )
