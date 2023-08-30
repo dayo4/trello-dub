2876590f86f085e6bc47daf823344ced22aeb168
SomarGuy <diogo.ramos@me.com>
2023-08-29 20:04:23 -0400

    create Boardtsx file & add to pagetsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
-      {/* Header */}
-      <Header/>
-      {/* Board */}
-      <h1>Trello Clone</h1>
+      <Header />
+
+      <Board />
     </main>
   )
 }
