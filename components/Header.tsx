c2003b8ae71d77b7e6af9b88b798a8be6cef9199
SomarGuy <diogo.ramos@me.com>
2023-08-29 20:03:09 -0400

    Introduce blue background gradient behind Trello logo, modify search box layout, and add Avatar to Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,25 @@ function Header() {
   return (
     <header>
         <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
+
+            <div
+                className='
+                absolute 
+                top-0
+                left-0
+                w-full
+                h-96
+                bg-gradient-to-br
+                from-blue-400
+                to-blue-700
+                rounded-md
+                filter
+                blur-3xl
+                opacity-50
+                -z-50
+                '
+            />
+
         <Image
             src="https://links.papareact.com/c2cdd5"
             alt="Trello Logo"
