682cfa9d542ec7f15a5b198d81c42c64c009400e
SomarGuy <diogo.ramos@me.com>
2023-09-07 01:12:12 -0400

    build column compnent

---
+++ components/Column.tsx
@@ -0,0 +1,38 @@
+import { Draggable, Droppable } from "react-beautiful-dnd"
+
+type Props = {
+    id: TypedColumn;
+    todos: Todo[];
+    index: number;
+};
+
+function Column({id, todos, index}: Props) {
+    return (
+        <Draggable draggableId={id} index={index}>
+            {(provided) => (
+                <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                >
+                    {/* Render Droppable */}
+                    <Droppable droppableId={index.toString()} type="card">
+                        {(provided, snapshot) => (
+                        <div
+                        {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            className={`p-2 rounded-2xl shadow-sm ${
+                                snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                            }`}
+                        >
+                            <h2>{id}</h2>
+                        </div>
+                        )}
+                    </Droppable>
+                </div>
+            )}
+        </Draggable>
+    )
+}
+
+export default Column;
---
+++ store/BoardStore.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand'
+import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
+
+interface BoardState {
+    board: Board;
+    getBoard: () => void
+}
+
+export const useBoardStore = create<BoardState>((set) => ({
+    board: {
+        columns: new Map<TypedColumn, Column>()
+    },
+    getBoard: async() => {
+        const board = await getTodosGroupedByColumn();
+        set({ board });
+    }
+}))
+
