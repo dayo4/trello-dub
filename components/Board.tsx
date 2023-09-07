ca6049673f0a6d872965c19c77b57c1e91b5d02f
SomarGuy <diogo.ramos@me.com>
2023-09-07 19:25:07 -0400

    Extend drag-and-drop to support reordering todos within and between columns

--- components/Board.tsx
+++ components/Board.tsx
@@ -28,11 +28,44 @@ function Board() {
       setBoardState({
         ...board,
         columns: rearrangedColumns
-      })
+      });
+    }
+
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    if (!startCol || !finishCol) return;
+
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      
     }
   };
-
-  console.log(board)
   return (
   <DragDropContext onDragEnd={handleOnDragEnd}>
     <Droppable droppableId="board" direction="horizontal" type="column">
