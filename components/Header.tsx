a51d439ba2ef5f22fd9dc65264154b32a853a90f
SomarGuy <diogo.ramos@me.com>
2023-09-08 11:58:29 -0400

    install headlessUI

--- components/Header.tsx
+++ components/Header.tsx
@@ -82,7 +82,7 @@ function Header() {
         </div>
 
         <div className='flex items-center justify-center px-5 py-2 md:py-5'>
-            <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]'>
+        <p className='flex items-center p-5 text-sm font-semibold pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-black border border-gray-300'>
                 <UserCircleIcon 
                 className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${
                     loading && "animate-spin"
