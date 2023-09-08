809a12e677aafd466451a681ff99134655bb2ac5
SomarGuy <diogo.ramos@me.com>
2023-09-07 20:30:59 -0400

    Add database update operation and integrate into Board drag-n-drop

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,10 +5,11 @@ import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd'
 import Column from './Column';
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore((state) => [
     state.board,
     state.getBoard,
     state.setBoardState,
+    state.updateTodoInDB,
   ]);
 
   useEffect(() => {
@@ -78,6 +79,8 @@ function Board() {
         id: finishCol.id,
         todos: finishTodos,
       });
+
+      updateTodoInDB(todoMoved, finishCol.id);
 
       setBoardState({ ...board, columns: newColumns });
     }
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,10 +1,12 @@
 import { create } from 'zustand'
 import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
+import { databases } from '@/appwrite';
 
 interface BoardState {
     board: Board;
     getBoard: () => void
     setBoardState: (board: Board) => void;
+    updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -17,5 +19,17 @@ export const useBoardStore = create<BoardState>((set) => ({
     },
 
     setBoardState: (board) => set({ board }),
+    
+    updateTodoInDB: async(todo, columnId) => {
+        await databases.updateDocument(
+            process.env.NEXT_PUBLIC_DATABASE_ID!,
+            process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+            todo.$id,
+            {
+                title: todo.title,
+                status: columnId
+            }
+        )
+    },
 }))
 
